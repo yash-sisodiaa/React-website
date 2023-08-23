c218ad092c18ef034c78dfe12bcbb7f936570feb
Yash sisodia <128230794+yash-sisodiaa@users.noreply.github.com>
2023-08-23 21:25:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+
+
+import "./styles/App.scss"
+import "./styles/header.scss"
+import "./styles/home.scss"
+import "./styles/footer.scss"
+import "./styles/Contact.scss"
+import "./styles/mediaquery.scss"
+
+
+
+
+
+
+
+
+function App() {
+  return (
+<Router>
+     <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/services" element={<Services/>} />
+
+      </Routes>
+      <Footer/>
+      <Routes/>
+</Router>
+  );
+}
+
+export default App;
